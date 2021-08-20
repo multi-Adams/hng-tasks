4c984863fe7d2b7a8f053e3cc33e1d38ecce82bc
multi-Adams <62416019+multi-Adams@users.noreply.github.com>
2021-08-20 15:17:39 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,40 +1,19 @@
 <?php
 $q = $_REQUEST['q'];
 $req = json_decode($q, true);
-// $msg = [];
-// $msg['name'] = $q['name'];
-// $msg['email'] = $q['email'];
-// $msg['subject'] = $q['subject'];
-// $msg['msg'] = $q['message'];
-
-// echo (json_encode($q));
-
-
-// echo $msg;
-
-// print_r($msg);
-
-
 
 function send_to_db($data)
 {
-
-
   $host = "localhost";
   $dbUser = "ade";
   $password = "ade123";
   $database = "msgs";
-
-
-
 
   $connection = new mysqli($host, $dbUser, $password, $database);
 
   if ($connection->connect_error) {
     die("Database Connection Error, Error No.: " . $connection->connect_errno . " | " . $connection->connect_error);
   }
-
-
 
   $name = $data['name'];
   $email = $data['email'];
@@ -46,7 +25,3 @@ function send_to_db($data)
 }
 
 send_to_db($req);
-
-
-
-// print_r($req);
